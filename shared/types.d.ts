70a9c7f1a9656acd92c3d485e9fa6281aa9922ee
Sirui Yao <20095250@mail.wit.ie>
2024-03-12 21:50:52 +0000

    Add Auth API and Configure the POST and PUT endpoints of the App API as protected routes.

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -36,3 +36,18 @@ export type MovieReviews = {
   Content: string;
   Rating: number
   };
+  export type SignUpBody = {
+    username: string;
+    password: string;
+    email: string
+  }
+  
+  export type ConfirmSignUpBody = {
+    username: string;
+    code: string;
+  }
+  
+  export type SignInBody = {
+    username: string;
+    password: string;
+  }
